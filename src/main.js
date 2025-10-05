6d8149338205c341ed955749cac8a3835f030f0e
grcn-sme <143241020+grcn-sme@users.noreply.github.com>
2025-10-06 01:51:25 +0800

    add cleanup logic, to remove injected code during page navigation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
 // custom code injector 
 // html code to inject dynamically on run time
-function injectCustomCode(htmlCode, intoElement) { let addText; if (intoElement === document.head) { const nodeToAdd = []; addText = (TAG) => { nodeToAdd.push(new Text(TAG.textContent)); setTimeout(_ => { document.body.prepend(nodeToAdd.pop()); }); }; } else { addText = (TAG) => { intoElement.appendChild(new Text(TAG.textContent)); }; } const cloneChildNodes = (TAG) => { try { if (TAG.attributes === undefined) { if (TAG.nodeName === "#text") { addText(TAG); return new Text(TAG.textContent); } else if (TAG.nodeName === "#comment") { intoElement.appendChild(new Comment(TAG.textContent)); } else { console.warn("unknown node type, assume as textNode instead: ", TAG); addText(TAG); } } else { const tag = document.createElement(TAG.tagName); for (const a of TAG.attributes) { tag.setAttribute(a.name, a.value); } tag.innerHTML = TAG.innerHTML; intoElement.appendChild(tag); } } catch (err) { console.warn(err); } }; const doc = new DOMParser().parseFromString(htmlCode, "text/html"); doc.head.childNodes.forEach(cloneChildNodes); doc.body.childNodes.forEach(cloneChildNodes); }
+// function injectCustomCode(htmlCode, intoElement) { let addText; if (intoElement === document.head) { const nodeToAdd = []; addText = (TAG) => { nodeToAdd.push(new Text(TAG.textContent)); setTimeout(_ => { document.body.prepend(nodeToAdd.pop()); }); }; } else { addText = (TAG) => { intoElement.appendChild(new Text(TAG.textContent)); }; } const cloneChildNodes = (TAG) => { try { if (TAG.attributes === undefined) { if (TAG.nodeName === "#text") { addText(TAG); return new Text(TAG.textContent); } else if (TAG.nodeName === "#comment") { intoElement.appendChild(new Comment(TAG.textContent)); } else { console.warn("unknown node type, assume as textNode instead: ", TAG); addText(TAG); } } else { const tag = document.createElement(TAG.tagName); for (const a of TAG.attributes) { tag.setAttribute(a.name, a.value); } tag.innerHTML = TAG.innerHTML; intoElement.appendChild(tag); } } catch (err) { console.warn(err); } }; const doc = new DOMParser().parseFromString(htmlCode, "text/html"); doc.head.childNodes.forEach(cloneChildNodes); doc.body.childNodes.forEach(cloneChildNodes); }
+
+/** 
+ * @param {string} htmlCode
+ * @param {HTMLElement} intoElement
+ */
+function injectCustomCode(htmlCode, intoElement) {
+  const b = document.createElement('template');
+  b.innerHTML = htmlCode;
+  const nodes = [...(b.content.cloneNode(true).childNodes)];
+  const len = nodes.length;
+  for (let i = 0; i < len; ++i) {
+    const x = nodes[i];
+    if (x.nodeName === 'SCRIPT') {
+      const sc = document.createElement('script');
+      if (x.attributes) for (const a of x.attributes) {
+        sc.setAttribute(a.name, a.value);
+      };
+      sc.textContent = x.textContent;
+      intoElement.appendChild(sc);
+      nodes[i] = sc;
+      continue;
+    }
+    intoElement.appendChild(x);
+      nodes[i] = x;
+  }
+  window.addEventListener('popstate', function cleanup() {
+    const len = nodes.length;
+    for (let i = 0; i < len; ++i) {
+      nodes[i].remove();
+    }
+  }, { once: true, capture: true});
+  return nodes;
+}
+
 
 const innerHTMLPolicy = trustedTypes.createPolicy("passthrough", {
   createHTML: (html) => html,
@@ -208,7 +242,7 @@ async function loadImageFile(url) {
 /** @param {URLSearchParams} searchParams */
 function processSearchParams(searchParams) {
   const gdocsId = searchParams.get('serverUrlId');
-  console.log({searchParams, gdocsId});
+  console.log({ searchParams, gdocsId });
   if (gdocsId) {
     localStorage['txtRemoteCodeUrl'] = `https://docs.google.com/document/d/${gdocsId}/preview`;
   }
