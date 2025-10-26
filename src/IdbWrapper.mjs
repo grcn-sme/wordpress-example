4a40c14df7d196e83994fd43bfcbf6cc5f3576e2
grcn-sme <143241020+grcn-sme@users.noreply.github.com>
2025-10-26 15:55:30 +0800

    update idb example

--- src/IdbWrapper.mjs
+++ src/IdbWrapper.mjs
@@ -1,3 +1,16 @@
+class IdbObjectStoreWrapper {
+    /** @type {IDBObjectStore} */
+    #objectStore;
+    constructor(objectStore) {
+        this.#objectStore = objectStore;
+    }
+
+    get indexName() {
+        return this.#objectStore.index('indexName');
+    }
+}
+
+
 export class IdbWrapper {
 
     /** @type {IDBDatabase} */
@@ -7,12 +20,15 @@ export class IdbWrapper {
         this.#db = db;
     }
 
+
     /** 
+     * open connection to an IndexedDB, return in a form of wrapped IndexedDB
      * @param {string} dbName
      * @param {number} version
+     * @param {(db: IDBDatabase, existingObjectStoreNames: DOMStringList) => void} setup_db_structure
      * @returns {Promise<IdbWrapper>}
      */
-    static open(dbName, version) {
+    static open(dbName, version, setup_db_structure) {
         return new Promise((resolve, fail) => {
             const request = window.indexedDB.open(dbName, version);
             request.onerror = fail;
@@ -21,6 +37,7 @@ export class IdbWrapper {
                 /** @type {IDBDatabase} */
                 const db = event.target.result;
                 console.log({ db });
+                db.onversionchange = db.close;
                 const dbw = new IdbWrapper(db);
                 resolve(dbw);
             };
@@ -29,10 +46,8 @@ export class IdbWrapper {
             request.onupgradeneeded = function (ev) {
                 /** @type {IDBDatabase} */
                 const db = ev.target.result;
-
-                const tbl = db.createObjectStore('lsa');
-                console.log({ tbl });
-
+                db.onversionchange = db.close;
+                setup_db_structure(db, db.objectStoreNames);
             };
 
             request.onblocked = function (ev) {
@@ -43,6 +58,23 @@ export class IdbWrapper {
                 alert('please close other tabs to allow upgrade');
             };
         });
+    }
+
+    /**
+     * @param {IDBDatabase} db
+     * @param {string} objectStoreName
+     * @param {IDBObjectStoreParameters} options
+     * @returns {IDBObjectStore?} the newly created IDBObjectStore, otherwise null if there is existing
+     */
+    static createObjectStore(db, objectStoreName, options) {
+        if (db.objectStoreNames.contains(objectStoreName) === true) return null;
+        else return db.createObjectStore(objectStoreName, options);
+    }
+
+    close() {
+        // close the database. This allows the other page to upgrade the database.
+        this.#db.close();
+        console.log("A new version of this page is ready. Please reload or close this tab!");
     }
 
     /** 
@@ -65,9 +97,9 @@ export class IdbWrapper {
 
     /** 
      * @param {string} objectStoreName
-     * @param {Iterator<[key: (string | number | object), value: any]>} iterator
+     * @param {object[]} items
     */
-    add(objectStoreName, iterator) {
+    add(objectStoreName, items) {
         return new Promise((resolve, fail) => {
             const db = this.#db;
             const transaction = db.transaction(objectStoreName, 'readwrite');
@@ -77,23 +109,18 @@ export class IdbWrapper {
                 fail(ev.target.error);
             };;
 
-            const tbl = transaction.objectStore(objectStoreName);
-            for (
-                let r = iterator.next();
-                r.done === false;
-                r = iterator.next()
-            ) {
-                const x = r.value;
-                tbl.add(x[1], x[0]);
+            const tbl = transaction.objectStore(objectStoreName), len = items.length;
+            for (let i = 0; i < len; ++i) {
+                tbl.add(items[i]);
             }
         });
     }
 
     /** 
      * @param {string} objectStoreName
-     * @param {Iterator<[key: (string | number | object), value: any]>} iterator
+     * @param {object[]} items
     */
-    update(objectStoreName, iterator) {
+    set(objectStoreName, items) {
         return new Promise((resolve, fail) => {
             const db = this.#db;
             const transaction = db.transaction(objectStoreName, 'readwrite');
@@ -103,17 +130,34 @@ export class IdbWrapper {
                 fail(ev.target.error);
             };;
 
-            const tbl = transaction.objectStore(objectStoreName);
-            for (
-                let r = iterator.next();
-                r.done === false;
-                r = iterator.next()
-            ) {
-                const x = r.value;
-                tbl.put(x[1], x[0]);
+            const tbl = transaction.objectStore(objectStoreName), len = items.length;
+            for (let i = 0; i < len; ++i) {
+                tbl.put(items[i]);
             }
         });
+    }
 
+    /** 
+     * manually set key and values: object
+     * @param {string} objectStoreName
+     * @param {[key: (string | number | object), value: any]} kvPairs
+    */
+    setKeyValues(objectStoreName, kvPairs) {
+        return new Promise((resolve, fail) => {
+            const db = this.#db;
+            const transaction = db.transaction(objectStoreName, 'readwrite');
+            transaction.oncomplete = resolve;
+            transaction.onerror = (ev) => {
+                // console.warn(ev);
+                fail(ev.target.error);
+            };;
+
+            const tbl = transaction.objectStore(objectStoreName), len = kvPairs.length;
+            for (let i = 0; i < len; ++i) {
+                const kv = kvPairs[i];
+                tbl.put(kv[1], kv[0]);
+            }
+        });
     }
 
     /** 
@@ -200,6 +244,47 @@ export class IdbWrapper {
         });
     }
 
+
+    /** 
+     * find rows by the 'addon' indexed key
+     * @param {string} objectStoreName
+     * @param {string} indexName 
+     * @param {string} searchValue 
+    * */
+    readByIndex(objectStoreName, indexName, searchValue) {
+        return new Promise((resolve, fail) => {
+            const db = this.#db;
+            const tbl = db.transaction(objectStoreName, 'readonly').objectStore(objectStoreName);
+            console.log({ objectStoreName: objectStoreName, objectStore: tbl, indexNames: tbl.indexNames, searchValue: searchValue });
+            const index = tbl.index(indexName);
+            const request = index.get(searchValue);
+
+            request.onerror = (ev) => {
+                // console.warn(ev);
+                fail(ev.target.error);
+            };
+
+            request.onsuccess = (ev) => {
+                console.log({ result: ev.target.result });
+                resolve(ev.target.result);
+            };
+
+        });
+    }
+
+}
+
+/**
+ * example
+ * @param {IDBDatabase} db
+ * @param {string} objectStoreName
+*/
+function exampleObjectStore(db, objectStoreName) {
+    if (db.objectStoreNames.contains(objectStoreName) === false) {
+        const tbl = db.createObjectStore(objectStoreName, { keyPath: 'ssn' });
+        console.log({ tbl });
+        tbl.createIndex('indexName1', 'keyName1', { unique: true });
+    }
 }
 
 
--- src/page/indexeddb/_.js
+++ src/page/indexeddb/_.js
@@ -7,8 +7,17 @@ export default async function main(appBody) {
     // window.idbExample = idbExample;
     // setupUIexample(idbExample, appBody);
 
+
     /** @type {IdbWrapper} */
-    const idbWrapper = await IdbWrapper.open('haha');
+    const idbWrapper = await IdbWrapper.open('haha', 2, (db, existingStores) => {
+        if (db.objectStoreNames.contains('exampleStore') === false) {
+            const exampleStore = db.createObjectStore('exampleStore', { keyPath: 'uname' });
+            exampleStore.createIndex('userEmail', 'email', { unique: true });
+        }
+
+        const ost = IdbWrapper.createObjectStore(db, 'lsa', { keyPath: 'ssn' });
+        ost?.createIndex('indexName1', 'keyedName1', { unique: true });
+    });
     window.idbWrapper = idbWrapper;
     setupUI(idbWrapper, appBody);
 }
@@ -31,20 +40,52 @@ function setupUI(idbWrapper, appBody) {
 
     document.getElementById('btnAdd').addEventListener('click', async function (e) {
         idbWrapper.add('lsa', [
-            [Date.now(), Math.random() + ' ' + 'valu']
-        ].values());
+            {
+                ssn: 'inline key: ' + Math.random(),
+                keyedName1: 'indexed_key_' + Date.now()
+            }
+        ]);
+
+        idbWrapper.add('exampleStore', [
+            {
+                uname: 'inline key: ' + Date.now(),
+                email: 'addon_index_key__' + Math.random() + '@gmail.com',
+                name: 'test'
+            }
+        ]);
     });
 
     document.getElementById('btnUpdate').addEventListener('click', function (e) {
-        idbWrapper.update('lsa', [
-            [1, `updated value: ${(Date.now()).toString().split('').reverse().join('')}`]
-        ].values());
+        idbWrapper.set('lsa', [
+            { ssn: Date.now() }
+        ]);
     });
 
     document.getElementById('btnDelete').addEventListener('click', function (e) {
         idbWrapper.delete('lsa', [
-            1
+            1,
+            1761463513658
         ]);
+    });
+
+
+    document.getElementById('txtIndexedKey').addEventListener('change', function (e) {
+        e.target.setCustomValidity('');
+        document.getElementById('txtReadByIndexedKey').dispatchEvent(new InputEvent('input'));
+    });
+
+    document.getElementById('txtReadByIndexedKey').addEventListener('input', async function (e) {
+        /** @type {HTMLInputElement} */
+        const txtIndexName = document.getElementById('txtIndexedKey');
+        try {
+            const indexName = txtIndexName.value;
+            await idbWrapper.readByIndex('lsa', indexName,
+                e.target.value
+            );
+        } catch (err) {
+            txtIndexName.setCustomValidity(err.toString());
+            txtIndexName.reportValidity();
+        }
     });
 }
 
